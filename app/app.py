275b726d7399417ca01f2810a2289a40f5ff3f42
Cédrine Monnet <monnetcedrine@team-lab.com>
2024-01-12 19:48:56 +0900

    dockerize + frontend react demo

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 import pandas as pd
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from city_pop_generator import format_special_character, generate_lyrics
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/", methods = ['GET'])
+@app.route("/api/v1", methods = ['GET'])
 def generate():
     df = pd.read_csv("lyrics.csv")
     pd.set_option("display.max_colwidth", 1000)
@@ -19,8 +21,8 @@ def generate():
 
     formatted_text = format_special_character(text)
     generated_lyrics = generate_lyrics(formatted_text)
-    return generated_lyrics
+    return jsonify({"lyrics": generated_lyrics})
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=True, port=8000)
+    app.run(host="0.0.0.0", debug=True, port=8000)
